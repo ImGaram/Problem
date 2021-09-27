2cd9289db45bf0e346d2e85a6eedc7a35645dc5d
imgaram <s21049@gsm.hs.kr>
2021-09-28 08:31:45 +0900

    codeup

---
+++ codeup/논리연산/Q1053.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main()
+{
+	//코드업 1019번
+	/*int d, y, t;
+	scanf("%d.%d.%d", &y ,&d ,&t);	//%4d : 4칸 띄어 출력
+	printf("%04d.%02d.%02d", y, d, t);*/  //%04d : 4칸 띄어 출력(숫자가 4자리 이하일땐 0도 출력 예 : 4 -> 0004)
+
+	int a;
+	scanf("%d", &a);
+	if (a == 1) {
+		printf("%d", !a);
+	}
+	else if (a == 0)
+		printf("%d", !a);
+}
