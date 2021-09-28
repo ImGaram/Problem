d7758e212ec9110dfc47c6ccd96d5b88888a35e2
imgaram <s21049@gsm.hs.kr>
2021-09-28 15:23:19 +0900

    baekjoon if kotlin

---
+++ BOJ/baekjoon-kotlin/chapter2/Q1330.kt
@@ -0,0 +1,15 @@
+package chapter2
+
+import java.util.*
+
+fun main(args:Array<String>){
+    val sc: Scanner = Scanner(System.`in`);
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    if (a > b)
+        print(">")
+    else if (a<b)
+        print("<")
+    else
+        print("==")
+}
