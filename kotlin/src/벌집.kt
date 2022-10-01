25e17bad48587be63b52452a885fd7da235fb8fb
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-01 22:02:25 +0900

    2292번 벌집

--- kotlin/src/벌집.kt
+++ kotlin/src/벌집.kt
@@ -1,3 +1,25 @@
-fun main() {
+import java.util.Scanner
 
+fun main() {
+    val sc = Scanner(System.`in`)
+    val number = sc.nextInt()
+    var honeyComb = 1
+    var beforeHoneyComb: Int
+    var honeyCombCnt = 1
+
+    while (true) {
+        beforeHoneyComb = honeyComb
+        honeyComb += honeyCombCnt * 6  // 1 = 1 * 6 + 1
+        honeyCombCnt++  // 돈 횟수
+
+        if (number == 1) {  // 1 예외처리
+            println(1)
+            break
+        }
+
+        if (beforeHoneyComb + 1 <= number && number <= honeyComb) {
+            println(honeyCombCnt)
+            break
+        } else continue
+    }
 }
