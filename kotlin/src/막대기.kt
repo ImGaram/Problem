e8d52bf9293d7241aeabe412e98f127436a5f6ce
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-10 14:00:06 +0900

    백준 막대기(성공)

--- kotlin/src/막대기.kt
+++ kotlin/src/막대기.kt
@@ -1,39 +1,24 @@
-import java.util.Scanner
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 fun main() {
-    val sc = Scanner(System.`in`)
-    val stickSize = sc.nextInt()
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val cm = bf.readLine().toInt()
+    bf.close()
 
     var stick = 64
-    var totalStick = 0
-    var cutCount = 0
-    var tempStick = stickSize
-
-/*    while (totalStick < stick) {
-        while (stick > stickSize) {
+    var cut = 0
+    var total = 0
+    var temp = cm
+    while (total < cm) {
+        while (stick > temp) {
             stick/=2
         }
-        println(stick)      // 16
-        stickSize-=stick    // 7
-        totalStick+=stick
-        println(stickSize)
-        cutCount+=1
+
+        total+=stick
+        temp-=stick
+        cut+=1
     }
 
-    println(cutCount)*/
-
-    // 값 입력 -> 64에서 나누기 2로 -> 나눈 값 더하기 -> 반복 2~3 -> 같거나 커지면 종료 -> 출력
-    while (totalStick <= tempStick) {   // 16 20 22 23
-        while (stick >= tempStick) { // 64 32 16 8 4 2
-            stick/=2
-        }
-        // 16 + 4 + 2 + 1
-        totalStick+=stick
-        cutCount+=1
-        tempStick = stick
-
-        println("temp stick: $tempStick")
-        println("$stick $totalStick")
-    }
-    println(cutCount)
+    println(cut)
 }
