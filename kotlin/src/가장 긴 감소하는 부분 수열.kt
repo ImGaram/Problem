87a3761dda0580aa6bf643f56b47cedd84f2f1de
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-19 10:16:04 +0900

    백준 가장 긴 감소하는 부분 수열

--- kotlin/src/가장 긴 감소하는 부분 수열.kt
+++ kotlin/src/가장 긴 감소하는 부분 수열.kt
@@ -7,46 +7,21 @@ fun main() {
     val arrIndex = bf.readLine().toInt()
     val st = StringTokenizer(bf.readLine())
 
-    val array = arrayListOf<Int>()
-    repeat(arrIndex) {
-        if (st.hasMoreTokens())
-            array.add(st.nextToken().toInt())
+    val array = IntArray(arrIndex)
+    repeat(arrIndex) { i ->
+        array[i] = st.nextToken().toInt()
     }
 
-    var big = 0
-    var bigIndex = 0
-    val distinctArray = array.distinct()
-    for (i in distinctArray.indices) {
-        if (array[i] > big) {
-            big = array[i]
-            bigIndex = i
+    val res = IntArray(arrIndex)
+    for (i in 0 until arrIndex) {
+        if (res[i] == 0) res[i] = 1
+        for (j in 0 until arrIndex) {
+            // array[i]보다 큰 경우
+            if (array[i] < array[j]) {
+                if (res[i] < res[j]+1) res[i] = res[j] + 1
+            }
         }
     }
-    //7
-    //5 4 3 2 30 20 1
-
-    //5
-    //7 5 3 1 10
-
-    //6
-    //10 30 10 20 20 10
-
-    val tempArr = arrayListOf<Int>()
-    for (i in bigIndex until array.size) {
-        tempArr.add(array[i])
-    }
-
-    println(tempArr.sortedDescending().distinct().size)
-
-    /*var bigCnt = 1
-    var data = 0
-    for (i in bigIndex until tempArr.distinct().size) {
-        if (array[i] < array[bigIndex]) {
-            if (data == array[i]) continue
-            bigCnt += 1
-            data = array[i]
-        }
-    }
-
-    println(bigCnt)*/
+    res.sort()
+    println(res[arrIndex-1])
 }
