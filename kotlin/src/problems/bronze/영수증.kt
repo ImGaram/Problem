54ad9a973bcc14ba5c09b2d16e3d6439dfe3c4e6
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-29 00:54:34 +0900

    백준 문제 2개

---
+++ kotlin/src/problems/bronze/영수증.kt
@@ -0,0 +1,21 @@
+package problems.bronze
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val total = sc.nextInt()
+    val kind = sc.nextInt()
+    var totalPrice = 0
+
+    repeat(kind) {
+        val price = sc.nextInt()
+        val cnt = sc.nextInt()
+
+        totalPrice+=price*cnt
+    }
+
+    if (total == totalPrice) println("Yes")
+    else println("No")
+}
