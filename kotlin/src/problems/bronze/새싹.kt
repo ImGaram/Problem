3aed7cf09eb300f5d34fb4856b583ccea96710e0
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-28 17:11:17 +0900

    백준 문제 2개

---
+++ kotlin/src/problems/bronze/새싹.kt
@@ -0,0 +1,10 @@
+package problems.bronze
+
+fun main() {
+    println("         ,r'\"7")
+    println("r`-_   ,'  ,/")
+    println(" \\. \". L_r'")
+    println("   `~\\/")
+    println("      |")
+    println("      |")
+}
