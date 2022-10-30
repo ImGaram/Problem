e760ce3bf8b1b32f1633a3078c5fd0b533f6a2cb
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-30 18:58:07 +0900

    백준 분해합

---
+++ kotlin/src/problems/bronze/분해합.kt
@@ -0,0 +1,23 @@
+package problems.bronze
+
+import kotlin.system.exitProcess
+
+fun main() {
+    val num = readln().toInt()
+
+    for (i in 1 .. 1000000) {
+        var temp = i
+        var total = 0
+        while (temp != 0) {
+            total+=(temp%10)
+            temp /=10
+        }
+
+        if (i + total == num) {
+            println(i)
+            exitProcess(0)
+        }
+    }
+
+    println(0)
+}
