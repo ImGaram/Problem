7b6a3d1644d3123209d10c57deb57223d9a94b42
ImGaram <gim78014@gmail.com>
2024-06-07 00:15:14 +0900

    :sparkles: :: 백준 silver 3 조합(bigInteger)

--- kotlin/src/problems/silver/조합.kt
+++ kotlin/src/problems/silver/조합.kt
@@ -7,12 +7,12 @@ fun main() = with(Scanner(System.`in`)) {
     val m = nextInt()
 
     var factorialN = 1L.toBigInteger()
-    for (i in m+1 .. n) {
+    for (i in n-m+1 .. n) {
         factorialN *= i.toBigInteger()
     }
 
     var factorialM = 1L.toBigInteger()
-    for (i in 1 .. n-m) {
+    for (i in 1 .. m) {
         factorialM *= i.toBigInteger()
     }
 
