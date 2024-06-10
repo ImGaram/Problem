e0f1d992967856e586559bdbab873eee0d94442f
ImGaram <gim78014@gmail.com>
2024-06-10 14:23:34 +0900

    :sparkles: :: 백준 bronze 1 도비의 난독증 테스트

---
+++ kotlin/src/problems/bronze/도비의 난독증 테스트.kt
@@ -0,0 +1,18 @@
+package problems.bronze
+
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    while (true) {
+        val case = nextInt()
+        if (case == 0) break
+
+        val strMap = mutableMapOf<String, String>()
+        for (i in 0 until case) {
+            val input = next()
+            strMap[input.lowercase()] = input
+        }
+
+        println(strMap[strMap.minOf { it.key }])
+    }
+}
