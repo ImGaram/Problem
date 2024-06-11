ad7aed1347238aacc9ad4b3e5f8a7bfa8b767418
ImGaram <gim78014@gmail.com>
2024-06-11 20:35:29 +0900

    :sparkles: :: 백준 bronze 1 택시 거리

---
+++ kotlin/src/problems/bronze/택시 거리.kt
@@ -0,0 +1,18 @@
+package problems.bronze
+
+import java.util.Scanner
+import kotlin.math.abs
+
+fun main() = with(Scanner(System.`in`)) {
+    val height = nextInt()
+    val width = nextInt()
+
+    val oneList = mutableListOf<List<Int>>()
+    for (h in 0 until height) {
+        for (w in 0 until width) {
+            if (nextInt() == 1) oneList.add(listOf(h, w))
+        }
+    }
+
+    println(abs(oneList[1][0] - oneList[0][0]) + abs(oneList[1][1] - oneList[0][1]))
+}
