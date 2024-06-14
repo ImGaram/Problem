90d71115ed7f1cdf74e946e6e7ef67a0dd68dcd1
ImGaram <gim78014@gmail.com>
2024-06-14 23:31:22 +0900

    :sparkles: :: 백준 silver 4 보물

---
+++ kotlin/src/problems/silver/보물.kt
@@ -0,0 +1,21 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val treasureCnt = readLine().toInt()
+    val treasureA = readLine().split(" ").map { it.toInt() }.toMutableList()
+    val treasureB = readLine().split(" ").map { it.toInt() }.toMutableList()
+
+    treasureB.sort()
+
+    var price = 0
+    for (i in 0 until treasureCnt) {
+        val maxA = treasureA.max()
+        price += treasureB[i] * maxA
+        treasureA.remove(maxA)
+    }
+
+    println(price)
+}
