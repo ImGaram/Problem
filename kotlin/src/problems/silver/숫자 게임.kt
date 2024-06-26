bb1cf303c88d7e48d806091e4012cb9def3b643f
ImGaram <gim78014@gmail.com>
2024-06-26 12:07:08 +0900

    :sparkles: :: 백준 silver 5 숫자 게임(경우의 수 알고리즘)

---
+++ kotlin/src/problems/silver/숫자 게임.kt
@@ -0,0 +1,28 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val people = readLine().toInt()
+    val maxList = mutableListOf<Int>()
+
+    for (i in 0 until people) {
+        val cards = readLine().split(" ").map { it.toInt() }
+        var max = 0
+
+        for (i0 in cards.indices) {
+            for (i1 in i0 + 1 until cards.size) {
+                for (i2 in i1 + 1 until cards.size) {
+                    val cardSum = cards[i0] + cards[i1] + cards[i2]
+                    val lastNum = cardSum.toString().last().digitToInt()
+                    if (max < lastNum) max = lastNum
+                }
+            }
+        }
+
+        maxList.add(max)
+    }
+
+    println(maxList.indexOfLast { it == maxList.max() }+1)
+}
