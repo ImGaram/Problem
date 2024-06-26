a7439b27871232cf09794cca764bf02f4f30eebb
ImGaram <gim78014@gmail.com>
2024-06-26 09:01:02 +0900

    :sparkles: :: 백준 bronze 5 크냐?

---
+++ kotlin/src/problems/bronze/크냐.kt
@@ -0,0 +1,14 @@
+package problems.bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    while (true) {
+        val (a, b) = readLine().split(" ").map { it.toInt() }
+        if (a == 0 && b == 0) break
+
+        if (a > b) println("Yes")
+        else println("No")
+    }
+}
