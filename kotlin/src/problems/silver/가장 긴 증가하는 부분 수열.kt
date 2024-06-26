6ab7fc91b83be0d3a7a67e1f61f5723337331a03
ImGaram <gim78014@gmail.com>
2024-06-26 20:53:41 +0900

    :sparkles: :: 백준 silver 2 가장 긴 증가하는 부분 수열(dp)

---
+++ kotlin/src/problems/silver/가장 긴 증가하는 부분 수열.kt
@@ -0,0 +1,21 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val case = readLine().toInt()
+    val arr = Array(case) { 1 }
+    val nums = readLine().split(" ").map { it.toInt() }
+
+    for (i in 0 until case) {
+        for (j in 0 until i) {
+            if (nums[i] > nums[j]) {
+                arr[i] = max(arr[i], arr[j] + 1)
+            }
+        }
+    }
+
+    println(arr.max())
+}
