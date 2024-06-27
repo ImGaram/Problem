9e2016f8dcb9633e36e86209834119812e7f705b
ImGaram <gim78014@gmail.com>
2024-06-27 13:56:55 +0900

    :sparkles: :: 백준 gold 2 가장 긴 증가하는 부분 수열 2(이분 탐색)

---
+++ kotlin/src/problems/gold/가장 긴 증가하는 부분 수열 2.kt
@@ -0,0 +1,47 @@
+package problems.gold
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+private var sequence = mutableListOf<Int>()
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val size = readLine().toInt()
+    val input = StringTokenizer(readLine())
+
+    sequence.add(input.nextToken().toInt())
+    for (i in 1 until size) {
+        val number = input.nextToken().toInt()
+        if (number > sequence.last()) {
+            sequence.add(number)
+        } else {
+            binarySearch(0, sequence.size -1, number)
+        }
+    }
+
+    println(sequence.size)
+}
+
+private fun binarySearch(start: Int, end: Int, number: Int) {
+    val mid = (start + end) / 2
+    if (start > end) {
+        sequence[start] = number
+        return
+    }
+
+    // mid가 number보다 크다면?
+    // mid 이후의 값들은 정답이 아님.
+    if (sequence[mid] > number) {
+        binarySearch(start, mid - 1, number)
+    }
+    // mid가 number보다 작거나 같으면?
+    // mid포함 이전의 값들은 정답이 아님.
+    else if (sequence[mid] < number) {
+        binarySearch(mid+1, end, number)
+    }
+    else {
+        sequence[mid] = number
+        return
+    }
+}
