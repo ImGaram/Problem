c7808d6ea2696d84bd921f38c4a6f7a77b4df2da
ImGaram <gim78014@gmail.com>
2024-07-15 10:32:49 +0900

    :sparkles: :: 백준 Platinum 5 요세푸스 문제 3(dp)

---
+++ kotlin/src/problems/platinum/요세푸스 문제 3.kt
@@ -0,0 +1,15 @@
+package problems.platinum
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (n, k) = readLine().split(" ").map { it.toInt() }
+    var data = 1
+
+    for (i in 1 .. n) {
+        data = (data + k - 1) % i + 1
+    }
+
+    println(data)
+}
