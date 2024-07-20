a6bffafa98b6d7a7a6fd19f8450c88dd94b54f32
ImGaram <gim78014@gmail.com>
2024-07-20 21:34:14 +0900

    :sparkles: :: 백준 Silver 3 구간 합 구하기(누적 합)

---
+++ kotlin/src/problems/silver/구간 합 구하기 4.kt
@@ -0,0 +1,26 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (numCnt, case) = readLine().split(" ").map { it.toInt() }
+    val nums = readLine().split(" ").map { it.toInt() }
+    val prefixSum = Array(numCnt + 1) { 0 }
+
+    for (i in nums.indices) {
+        prefixSum[i + 1] = prefixSum[i] + nums[i]
+    }
+
+    for (i in 0 until case) {
+        val (start, end) = readLine().split(" ").map { it.toInt() }
+
+        bw.write("${prefixSum[end] - prefixSum[start - 1]}\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
