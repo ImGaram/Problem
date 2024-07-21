8ddb6c1bac413e3694ba3df01faa42db5485beae
ImGaram <gim78014@gmail.com>
2024-07-22 00:18:26 +0900

    :sparkles: :: 백준 Silver 2 색종이 만들기(재귀, 분할 정복)

---
+++ kotlin/src/problems/silver/색종이 만들기.kt
@@ -0,0 +1,46 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private var map = arrayOf<List<Int>>()
+private var blueCutCnt = 0
+private var whiteCutCnt = 0
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val mapSize = readLine().toInt()
+    map = Array(mapSize) { List(mapSize) { -1 } }
+
+    for (i in map.indices) {
+        map[i] = readLine().split(" ").map { it.toInt() }
+    }
+
+    confetti(0, 0, mapSize)
+
+    println(whiteCutCnt)
+    println(blueCutCnt)
+}
+
+private fun confetti(startX: Int, startY: Int, size: Int) {
+    var oneCnt = 0
+    var zeroCnt = 0
+
+    for (i in startX until size + startX) {
+        for (j in startY until size + startY) {
+            if (map[i][j] == 1) oneCnt++
+            else zeroCnt++
+        }
+    }
+
+    if (oneCnt == size * size) {
+        blueCutCnt++
+    } else if (zeroCnt == size * size) {
+        whiteCutCnt++
+    } else {
+        for (i in startX until size + startX step size / 2) {
+            for (j in startY until size + startY step size / 2) {
+                confetti(i, j, size / 2)
+            }
+        }
+    }
+}
