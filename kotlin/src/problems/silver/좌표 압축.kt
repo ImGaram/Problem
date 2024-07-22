2910c5b3f8db701b6baf23eb6bafd5a2e8cf6dcb
ImGaram <gim78014@gmail.com>
2024-07-22 09:52:40 +0900

    :sparkles: :: 백준 Silver 2 좌표 압축(정렬, 이분 탐색)

---
+++ kotlin/src/problems/silver/좌표 압축.kt
@@ -0,0 +1,21 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val case = readLine().toInt()
+    val list = readLine().split(" ").map { it.toInt() }
+    val sorted = list.distinct().sorted()
+
+    for (i in list.indices) {
+        bw.write("${sorted.binarySearch(list[i])} ")
+    }
+
+    bw.flush()
+    bw.close()
+}
