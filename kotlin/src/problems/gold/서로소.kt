27c481e58d801b21c4b888775b63c23f7f8cd7c7
ImGaram <gim78014@gmail.com>
2024-07-28 19:59:07 +0900

    :sparkles: :: 백준 Gold 1 서로소(오일러 피 함수)

---
+++ kotlin/src/problems/gold/서로소.kt
@@ -0,0 +1,34 @@
+package problems.gold
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.sqrt
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    while (true) {
+        var number = readLine().toInt()
+        if (number == 1) {
+            bw.write("0\n")
+            continue
+        }
+        if (number == 0) break
+
+        var res = number
+        for (i in 2 .. sqrt(number.toDouble()).toInt()) {
+            if (number % i == 0) {
+                res -= res / i
+                while (number % i == 0) number /= i
+            }
+        }
+
+        if (number > 1) res -= res / number
+        bw.write("${res}\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
