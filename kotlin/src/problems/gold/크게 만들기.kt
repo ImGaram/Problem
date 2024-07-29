615fc979864fb522397621e9168ced79e1b1359b
ImGaram <gim78014@gmail.com>
2024-07-29 13:06:50 +0900

    :sparkles: :: 백준 Gold 3 크게 만들기(그리디 알고리즘)

---
+++ kotlin/src/problems/gold/크게 만들기.kt
@@ -0,0 +1,24 @@
+package problems.gold
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.Stack
+
+private val stack = Stack<Int>()
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (addCnt, removeCnt) = readLine().split(" ").map { it.toInt() }
+    val number = readLine().map { it.digitToInt() }
+
+    var toRemoveCnt = 0
+    for (i in number.indices) {
+        while (stack.isNotEmpty() && toRemoveCnt != removeCnt && stack.peek() < number[i]) {
+            stack.pop()
+            toRemoveCnt++
+        }
+
+        if (stack.size < addCnt - removeCnt) stack.add(number[i])
+    }
+
+    println(stack.joinToString(""))
+}
