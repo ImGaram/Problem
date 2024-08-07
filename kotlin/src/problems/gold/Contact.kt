8a542046047f50027b4fa17bdbbb3bec8899ec80
ImGaram <gim78014@gmail.com>
2024-08-07 09:44:49 +0900

    :sparkles: :: 백준 Gold 5 Contact

---
+++ kotlin/src/problems/gold/Contact.kt
@@ -0,0 +1,16 @@
+package problems.gold
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val case = readLine().toInt()
+    val regex = Regex("(100+1+|01)+")
+
+    for (i in 0 until case ) {
+        val code = readLine()
+
+        if (regex.matches(code)) println("YES")
+        else println("NO")
+    }
+}
