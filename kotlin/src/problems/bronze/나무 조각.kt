8b2fa53af025b0d45faf996d0c86ad1659d4dfb5
ImGaram <gim78014@gmail.com>
2024-08-14 09:22:36 +0900

    :sparkles: :: 백준 Bronze 1 나무 조각

---
+++ kotlin/src/problems/bronze/나무 조각.kt
@@ -0,0 +1,26 @@
+package problems.bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val pieces = readLine().split(" ").map { it.toInt() }.toMutableList()
+
+    while (true) {
+        var isChanged = false
+
+        for (i in 0 until pieces.size - 1) {
+            if (pieces[i] > pieces[i + 1]) {
+                val save = pieces[i]
+                pieces[i] = pieces[i + 1]
+                pieces[i + 1] = save
+
+                isChanged = true
+
+                println(pieces.joinToString(" "))
+            }
+        }
+
+        if (!isChanged) break
+    }
+}
