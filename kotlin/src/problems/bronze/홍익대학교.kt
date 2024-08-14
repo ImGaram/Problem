c2239b23e0eebf51252739eea5da546037efb6ba
ImGaram <gim78014@gmail.com>
2024-08-14 09:11:23 +0900

    :sparkles: :: 백준 Bronze 5 홍익대학교

---
+++ kotlin/src/problems/bronze/홍익대학교.kt
@@ -0,0 +1,9 @@
+package problems.bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val year = readLine().toInt()
+    println(year - 1946)
+}
