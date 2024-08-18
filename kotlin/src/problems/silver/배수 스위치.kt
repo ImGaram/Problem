3151b96579dff8c1b7b5fcf6319acb48a7882002
ImGaram <gim78014@gmail.com>
2024-08-18 16:21:47 +0900

    :sparkles: :: 백준 Silver 4 배수 스위치

---
+++ kotlin/src/problems/silver/배수 스위치.kt
@@ -0,0 +1,26 @@
+package problems.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val electricBulb = readLine().map { it.toString() }
+    val bulbs = mutableListOf("0")
+    bulbs.addAll(electricBulb)
+
+    var switchClick = 0
+    for (i in 1 until bulbs.size) {
+        if (bulbs[i] == "N") continue
+        else {
+            for (c in i until bulbs.size step i) {
+                bulbs[c] = if (bulbs[c] == "Y") "N" else "Y"
+            }
+            switchClick++
+        }
+
+        if (!bulbs.contains("Y")) break
+    }
+
+    if (bulbs.contains("Y")) println(-1)
+    else println(switchClick)
+}
