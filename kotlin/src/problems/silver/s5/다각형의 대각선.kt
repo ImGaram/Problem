0484fcc11cd0e9fc38a855546b614f3ef7c19715
ImGaram <gim78014@gmail.com>
2024-10-18 18:37:15 +0900

    :sparkles: :: 백준 Silver 5 다각형의 대각선(3049번)

---
+++ kotlin/src/problems/silver/s5/다각형의 대각선.kt
@@ -0,0 +1,9 @@
+package problems.silver.s5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    println(n * (n - 1) * (n - 2) * (n - 3) / 24)
+}
