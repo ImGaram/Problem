f4073b8fda49a0ff20950cb2b31586b9d09c0d11
ImGaram <gim78014@gmail.com>
2024-10-23 14:33:43 +0900

    :sparkles: :: 백준 Bronze 3 암호 만들기(21553번)

---
+++ kotlin/src/problems/bronze/b3/암호 만들기.kt
@@ -0,0 +1,12 @@
+package problems.bronze.b3
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val a = bf.readLine()
+    val p = bf.readLine()
+
+    println(p)
+}
