a394d30a1018fae3b9b110d7fdea971b4571581e
ImGaram <gim78014@gmail.com>
2024-11-25 23:26:57 +0900

    :sparkles: :: 백준 Silver 4 조화평균(2090번)

---
+++ kotlin/src/problems/silver/s4/조화평균.kt
@@ -0,0 +1,29 @@
+package problems.silver.s4
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val n = bf.readLine().toInt()
+    val list = bf.readLine().split(" ").map { it.toLong() }
+    var numerator = 0L       // 분자
+    var denominator = 1L     // 분모
+
+    for (i in list.indices) {
+        val gcd = gcd(denominator, list[i])
+        denominator = (denominator * list[i]) / gcd
+    }
+
+    for (i in list.indices) {
+        numerator += denominator / list[i]
+    }
+
+    val reduction = gcd(denominator, numerator)
+    println("${denominator / reduction}/${numerator / reduction}")
+}
+
+private fun gcd(a: Long, b: Long): Long {
+    return if (b == 0L) a
+    else gcd(b, a % b)
+}
