aef87b4ff8dbf1175ce6c008ac5593695f38404b
ImGaram <gim78014@gmail.com>
2025-01-22 17:57:02 +0900

    :sparkles: :: 백준 Bronze 4 AI 선도대학(32951번)

---
+++ kotlin/src/problems/bronze/b5/AI 선도대학.kt
@@ -0,0 +1,11 @@
+package problems.bronze.b5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val n = bf.readLine().toInt()
+
+    println(n - 2024)
+}
