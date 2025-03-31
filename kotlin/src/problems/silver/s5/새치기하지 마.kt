94cd105b07b882d6ae84bfdc5f6f6707eaf12dd5
ImGaram <gim78014@gmail.com>
2025-03-31 14:47:19 +0900

    :sparkles: :: 백준 Silver 5 새치기하지 마!!!(33622번)

---
+++ kotlin/src/problems/silver/s5/새치기하지 마.kt
@@ -0,0 +1,12 @@
+package problems.silver.s5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val n = bf.readLine().toInt()
+
+    println(1)
+    println(n)
+}
