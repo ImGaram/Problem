8d1bd40b2a983b5021000631d25c7973c2e4110b
ImGaram <gim78014@gmail.com>
2025-04-11 16:17:18 +0900

    :sparkles: :: 백준 Bronze 5 SciComLove(21598번)

---
+++ kotlin/src/problems/bronze/b5/SciComLove.kt
@@ -0,0 +1,13 @@
+package problems.bronze.b5
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val n = bf.readLine().toInt()
+
+    repeat(n) {
+        println("SciComLove")
+    }
+}
